46c2e82b4448b9a6cec16e98dbd0e8e05d99ce14
Matt Thomson <red.cataclysm@gmail.com>
2015-09-17 15:01:57 -0400

    Implemented Complete All

--- app/client/templates/main/main.js
+++ app/client/templates/main/main.js
@@ -2,6 +2,19 @@
 /* Main: Event Handlers */
 /*****************************************************************************/
 Template.Main.events({
+  'click .toggle-all': function(event) {
+    // If all tasks are complete, set all incomplete
+    // Otherwise find the incomplete and set complete
+    if(!Tasks.find({completed: false}).count()) {
+      Tasks.find({}).forEach(function(task) {
+        Meteor.call('setComplete', task._id, false);
+      });
+    } else {
+      Tasks.find({completed: false}).forEach(function(task) {
+        Meteor.call('setComplete', task._id, true);
+      });
+    }
+  }
 });
 
 /*****************************************************************************/
