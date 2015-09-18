49852e83315a9249a9fc9570bbe86bfefcb2474d
Matt Thomson <red.cataclysm@gmail.com>
2015-09-18 11:03:03 -0400

    Fix: Blur not saving.
    Changed to focusout.

--- app/client/templates/task/task.js
+++ app/client/templates/task/task.js
@@ -12,13 +12,14 @@ Template.Task.events({
     console.log('Double Clicked Task');
     Session.set('editing', this._id);
   },
-  'keydown .edit, blur .edit': function(event) {
-    if(event.type === "keydown" && event.which === ESC_KEY || event.type === "blur") {
+  'keydown .edit, focusout .edit': function(event) {
+    console.log(event.type);
+    if(event.type === "keydown" && event.which === ESC_KEY) {
       console.log('Discarding Changes');
       event.target.value = this.title;
       Session.set('editing', null);
     } 
-    else if (event.which === ENTER_KEY) {
+    else if ((event.type === "keydown" && event.which === ENTER_KEY) || event.type === "focusout") {
       console.log('Saving Changes');
       Session.set('editing', null);
       Meteor.call('setTodo', this._id, event.target.value);
